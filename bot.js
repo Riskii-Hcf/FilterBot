d30122b479e457e675beee4b38327fe3d86a34a1
Riskii <34557522+Riskii-Hcf@users.noreply.github.com>
2018-05-11 12:45:10 +0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,254 @@
+const Discord = require('discord.js');
+ 
+const bot = new Discord.Client();
+ 
+const token = "Hell Nah";
+ 
+var prefix = '!';
+ 
+var eb = ["You can help youself right?",
+"Not right now Please",
+"Nope",
+"NOOOOOOO",
+"It is certain",
+"It is decidedly so",
+"Without a doubt",
+"Yes, definitely",
+"You may rely on it",
+"As I see it, yes",
+"Most likely",
+"Outlook good",
+"Yes",
+"Signs point to yes",
+"Reply hazy try again",
+"Ask again later",
+"Better not tell you now",
+"Cannot predict now",
+"Concentrate and ask again",
+"Don't count on it",
+"My reply is no",
+"My sources say no",
+"Outlook not so good",
+"Very doubtful"];
+
+var r = ["You can help youself right? or You Can't Kid?",
+"I couldn't hear the kid u were talking about because that person's name is SH*T **SPEAK UP**",
+"Your existence is the reason for scientists to rethink the plausibility of evolution",
+"I'm not trash talking, I'm talking to trash",
+"You're an inspiration for birth control",
+"Calling you a retard is a compliment in comparison to how stupid you actually are",
+"Go Kys U Loser Fag"];
+ 
+bot.on("ready", function() {
+      bot.user.setGame("PayBot2 Prefix = !" , "https://www.twitch.tv/123silly");
+    console.log(`Started bot as: ${bot.user.tag}!`);
+});
+ 
+bot.on("guildMemberAdd", function(member) {
+let joinleaves = member.guild.channels.find("name","main-chat");
+if(!joinleaves) return;
+joinleaves.sendMessage(member.toString() + " welcome to the server!");
+ 
+if(!joinleaves) return;
+ 
+});
+ 
+bot.on("guildMemberRemove", function(member) {
+ 
+let joinleaves = member.guild.channels.find("name","main-chat");
+ 
+if(!joinleaves) return;
+ 
+joinleaves.send(member.toString() + " has left... We will miss you! :cry:");
+});
+ 
+bot.on("message", function(message) {
+if (message.author.equals(bot.user)) return;
+ 
+if (!message.content.startsWith(prefix)) return;
+ 
+var args = message.content.substring(prefix.length).split(" ");
+ 
+switch (args[0].toLowerCase()) {
+
+  try{
+      if (!guilds[message.guild.id]) {
+          guilds[message.guild.id] = {
+              prefix: "(",
+              advert: 0,
+			  antiswear: 0,
+          };
+      }
+  } catch (e) {
+    console.log(e);
+  }
+
+  const advert = guilds[message.guild.id].advert;
+
+	if (advert === 1) { //detects if antiswear is on or not
+  var string = message.content;
+  var lower = string.toLowerCase();
+  for (i = 0; i < 554; i++)
+  {
+      if (lower.indexOf(ads.list[i]) >= 0)
+      {
+          if(message.guild.member(message.author).hasPermission(`ADMINISTRATOR`)) {
+			  //admins can advertise.
+         } else {
+          message.delete();
+          message.reply(":x: You are not allowed to advertise.")
+          //userData.swears++;
+          break;
+}
+      }
+  }
+}
+
+
+function clean(text) {
+  if (typeof(text) === `string`)
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, `@` + String.fromCharCode(8203));
+  else
+      return text;
+}
+
+const as = guilds[message.guild.id].as;
+
+if (as === 1) { //detects if antiswear is on or not
+  var string = message.content;
+  var lower = string.toLowerCase();
+  for (i = 0; i < 554; i++)
+  {
+      if (lower.indexOf(swears.list[i]) >= 0)
+      {
+          if(message.guild.member(message.author).hasPermission(`ADMINISTRATOR`)) {
+         } else {
+          message.delete();
+          message.reply(":x: You are not allowed to swear.")
+          //userData.swears++;
+          break;
+}
+      }
+  }
+}
+
+		
+case "ban":
+    let modRolesss = message.guild.roles.find("name", "Moderator");
+        if (args[1]) {
+    if(message.member.roles.has(modRolesss.id)) {
+      let banMember = message.guild.member(message.mentions.users.first());
+      message.guild.member(banMember).ban();
+      message.channel.sendMessage(":fire: You Have been banned on Riskii's Discord :fire:");
+      message.mentions.users.first().send(`:x: **You have been PERM BANNED! on ${message.guild.name}! :thinking: Please contact <@${message.author.id}> for more information!** :x:`)
+    } else {
+      return message.reply(":x: **You dont have permissions to do that!** :x:");
+    }
+    } else {
+    return message.reply(`:x: **Please mention a user** :x:`);
+    }
+break;
+case "mute":
+let modRolez = message.guild.roles.find("name", "Moderator");
+if(message.member.roles.has(modRolez.id)) {
+ let muteMember = message.guild.member(message.mentions.users.first());
+ muteMember.addRole(muteMember.guild.roles.find("name", "Muted"));
+ message.channel.sendMessage(":fire: You Have been Muted on Riskii's Discord :fire:");
+ message.mentions.users.first().send(`:x: **You have been MUTED on ${message.guild.name}! :thinking: Please contact <@${message.author.id}> for more information!** :x:`)
+}
+else {
+ return message.reply("=mute [player]");
+}
+break;
+case "unmute":
+let modRolezz = message.guild.roles.find("name", "Moderator");
+if (args[1]) {
+if(message.member.roles.has(modRolezz.id)) {
+let unMember = message.guild.member(message.mentions.users.first());
+unMember.removeRole(unMember.guild.roles.find("name", "Muted"));
+message.channel.sendMessage(":fire: You Have been Unmuted on Riskii's Discord :fire:");
+message.mentions.users.first().send(`:tada: **You have been unmuted on ${message.guild.name}! :wink: Please contact <@${message.author.id}> for more information!** :tada:`)
+} else {
+  return message.reply(":x: **You dont have permissions to do that!** :x:");
+}
+} else {
+return message.reply(`=unmute [player]`);
+}
+break;
+case "antiswear":
+if(message.guild.member(message.author).hasPermission(`ADMINISTRATOR`)) {
+if (args[1] === "on") {
+  guilds[message.guild.id].as=1; //sets it to 1
+  message.channel.sendMessage(`<:aloid_09:406932185371901964> **ANTI-SWEAR MODULE HAS BEEN TURNED ON!!**`)
+} else if (args[1] === "off") {
+  guilds[message.guild.id].as=0; //sets it to 0
+  message.channel.sendMessage(":x: **TURNED OFF ANTI-SWEAR MODULE** :x:")
+} else {
+  message.channel.sendMessage(":x: **INVALID USAGE!** Usage: ${prefix}antiswear on/off");
+}
+} else {
+	message.reply(":x: You have missing permissions: **ADMINISTATOR**! :x:");
+}
+break;
+case "warn":
+let modRoless = message.guild.roles.find("name", "Moderator");
+if (args[1]) {
+if(message.member.roles.has(modRoless.id)) {
+let wMember = message.guild.member(message.mentions.users.first());
+message.channel.sendMessage(":fire: You Have been Warned on Riskii's Discord :fire:");
+message.mentions.users.first().send(`:x: **You have been warned on ${message.guild.name}! :thinking: Please contact <@${message.author.id}> for more information!** :x:`)
+}
+else
+{
+  return message.reply(":x: **You dont have permissions to do that!** :x:");
+  } } else {
+      return message.reply(`=mute [player]`);
+  }
+break;
+case "unwarn":
+let modRolex = message.guild.roles.find("name", "Moderator");
+if (args[1]) {
+if(message.member.roles.has(modRolex.id)) {
+let uMember = message.guild.member(message.mentions.users.first());
+message.channel.sendMessage(":arrow_right: The Member Has Been Unwarned! :point_left:");
+ message.mentions.users.first().send(`:tada: **You have been unwarned on ${message.guild.name}! :wink: Please contact <@${message.author.id}> for more information!** :tada:`)
+}
+else
+{
+  return message.reply(":x: **You dont have permissions to do that!** :x:");
+}
+} else {
+break;
+case "clear":
+if (args[1]) {
+    if (args[1] > 2) {
+        if (args[1] > 50) {
+            message.reply("You are clearing too much messages!");
+        }
+        else {
+let messagecount = parseInt(args[1]);
+  message.channel.fetchMessages({limit: messagecount}).then(messages => message.channel.bulkDelete(messages));
+        }
+    } else {
+        message.reply("You can only delete 3 messages or more.");
+    }
+} else {
+    message.reply("Please type number of messages to delete.");
+}
+break;
+		
+	
+ 
+ 
+ 
+ 
+ 
+}
+});
+ 
+ 
+ 
+ 
+ 
+ 
+bot.login(process.env.BOT_TOKEN);
