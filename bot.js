ae6160a082d7e2d736a9ce25a01d1604811133a4
Riskii <34557522+Riskii-Hcf@users.noreply.github.com>
2018-05-13 05:35:03 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,13 +3,15 @@ const token = "Hell Nah";
 const bot = new Discord.Client();
 var prefix = '!';
 
+const antiswear = require("./modules/antiswear.js");
+
 bot.on("ready", function() {
       bot.user.setGame("FilterBot Prefix = !" , "https://www.twitch.tv/123silly");
     console.log(`Started bot as: ${bot.user.tag}!`);
 });
 
-case "chatfilter":
-message.reply(`{PREFIX}chatfilter ON/OFF`);
+case "antiswear":
+message.reply("{PREFIX}chatfilter ON/OFF");
 break;
 
 
