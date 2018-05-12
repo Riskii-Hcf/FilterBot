8700d1bce4482e0c13174f70bc867a31dbadca79
Riskii <34557522+Riskii-Hcf@users.noreply.github.com>
2018-05-13 05:24:53 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,39 +1,12 @@
-var Punishment = require('./punishment.js');
-var Logging = require('./logging.js');
-var Initialize = require('./initialize.js');
-
 const Discord = require('discord.js');
 const token = "Hell Nah";
 const bot = new Discord.Client();
-var bannedWords = Initialize.loadBannedWords();
+var prefix = '!';
 
-bot.on('ready', () => {
-    console.log('I am ready to make your server a better place!')
+bot.on("ready", function() {
+      bot.user.setGame("FilterBot Prefix = !" , "https://www.twitch.tv/123silly");
+    console.log(`Started bot as: ${bot.user.tag}!`);
 });
 
-bot.on('message', message => {
-    if(message.channel.type != "dm") {
-        if(!Punishment.checkPermissions(message.member)){
-            if(Punishment.checkProfanity(message.content, bannedWords)){
-                message.delete()
-                .then(msg => {                    
-                    Punishment.doleOutPunishment(bot, msg.member, msg.guild);
-                })
-            }
-        } 
-    }      
-});
-
-bot.on('messageDelete', message => {
-    Logging.logMessageDelete(bot, message);
-})
-
-bot.on('messageUpdate', (oldMessage, newMessage) => {
-    Logging.logMessageUpdate(bot, oldMessage, newMessage);
-})
-
-bot.on('guildBanAdd', (guild, user) =>{
-    Logging.logUserBan(bot, user, guild);
-})
 
 bot.login(process.env.BOT_TOKEN);
