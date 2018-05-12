90143c53b8d9a4ce1517ec963e886f283b89bc73
Riskii <34557522+Riskii-Hcf@users.noreply.github.com>
2018-05-13 05:38:10 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,43 @@ const Discord = require('discord.js');
 const token = "Hell Nah";
 const bot = new Discord.Client();
 var prefix = '!';
+const swears = require('./swears.js'); 
 
 bot.on("ready", function() {
       bot.user.setGame("FilterBot Prefix = !" , "https://www.twitch.tv/123silly");
     console.log(`Started bot as: ${bot.user.tag}!`);
 });
 
+case "antiswear":
+if(message.guild.member(message.author).hasPermission(`ADMINISTRATOR`)) {
+if (args[1] === "on") {
+  guilds[message.guild.id].as=1; //sets it to 1
+  message.channel.sendMessage(`**NOTICE** **ANTI-SWEAR MODULE HAS BEEN TURNED ON!!**`)
+} else if (args[1] === "off") {
+  guilds[message.guild.id].as=0; //sets it to 0
+  message.channel.sendMessage(":x: **TURNED OFF ANTI-SWEAR MODULE** :x:")
+} else {
+  message.channel.sendMessage(":x: **INVALID USAGE!** Usage: ${prefix}antiswear on/off");
+}
+} else {
+	message.reply(":x: You have missing permissions: **ADMINISTATOR**! :x:");
+}
+break;
+case "antiinvite":
+if(message.guild.member(message.author).hasPermission(`ADMINISTRATOR`)) {
+if (args[1] === "on") {
+  guilds[message.guild.id].advert=1; //sets it to 1
+  message.channel.sendMessage(`**NOTICE** **ANTI-DISCORD-LINKS MODULE HAS BEEN TURNED ON!!**`)
+} else if (args[1] === "off") {
+  guilds[message.guild.id].advert=0; //sets it to 0
+  message.channel.sendMessage(":x: **TURNED OFF ANTI-DISCORD-LINKS MODULE** :x:")
+} else {
+  message.channel.sendMessage(":x: **INVALID USAGE!** Usage: ${prefix}antiadvert on/off");
+}
+} else {
+	message.reply(":x: You have missing permissions: **ADMINISTATOR**! :x:");
+}
+break;
 
 
 bot.login(process.env.BOT_TOKEN);
